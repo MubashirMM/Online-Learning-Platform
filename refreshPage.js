69e54dbbe3164408264d6140262c63c00cadc5f7
MubashirMM <32304mubashir@gmail.com>
2025-02-26 15:28:27 +0500

    Updated Main to index

--- refreshPage.js
+++ refreshPage.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let currentPage = window.location.pathname.toLowerCase();
 
     // Redirect if the user is NOT on Main.html
-    if (!currentPage.endsWith("main.html")) {
-        window.location.replace("/Main.html");  // Ensure this is the correct path
+    if (!currentPage.endsWith("index.html")) {
+        window.location.replace("/index.html");  // Ensure this is the correct path
     }
 });
